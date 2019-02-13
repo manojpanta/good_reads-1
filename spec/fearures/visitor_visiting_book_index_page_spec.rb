273854c097e45eaa12d423683c10104acfc6f8bb
manojpanta <manojpanta95@gmail.com>
2019-02-12 19:15:33 -0700

    passing test for book user story 1

---
+++ spec/fearures/visitor_visiting_book_index_page_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'visitor visiting book index' do
+  it 'shows all the titles for books with link to book show page' do
+    book1 = Book.create(title: 'book1')
+    book2 = Book.create(title: 'book2')
+
+
+    visit '/books'
+
+    expect(page).to have_content(book1.title)
+    expect(page).to have_content(book2.title)
+
+    click_on book1.title
+
+    expect(current_path).to eq("/books/#{book1.id}")
+  end
+
+
+
+  # As a Visitor,
+  # When I visit a book index page,
+  # I see all book titles in the database,
+  # And each book title is a link to that book's show page.
+end
