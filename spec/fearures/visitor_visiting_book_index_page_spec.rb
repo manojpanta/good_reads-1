037f3f8dcbbb988d6bb353de3ea52ef172f192ad
manojpanta <manojpanta95@gmail.com>
2019-02-12 19:30:56 -0700

    user story 3 done

--- spec/fearures/visitor_visiting_book_index_page_spec.rb
+++ spec/fearures/visitor_visiting_book_index_page_spec.rb
@@ -23,3 +23,15 @@ describe 'visitor visiting book index' do
   # I see all book titles in the database,
   # And each book title is a link to that book's show page.
 end
+describe 'visitor visiting book show ' do
+  it ' shows list of reviews for the book' do
+    book1 = Book.create(title: 'book1')
+    user = User.create(name: 'user1')
+    review = book1.reviews.create(content: 'this is a review', rating: 4, user_id: user.id)
+
+    visit "/books/#{book1.id}"
+
+    expect(page).to have_content(book1.title)
+    expect(page).to have_content("#{review.content} by #{user.id}")
+  end
+end
