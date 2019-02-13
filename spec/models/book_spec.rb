004462ce1597b822adec8887a39180ff2471068a
manojpanta <manojpanta95@gmail.com>
2019-02-12 19:45:36 -0700

    added model test for highest rating

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -15,5 +15,18 @@ describe Book, type: :model do
 
       expect(average_rating).to eq((review1.rating + review2.rating + review3.rating)/3)
     end
+    it 'Highest rating' do
+
+      book1 = Book.create(title: 'book1')
+
+      user = User.create(name: 'user1')
+      review1 = book1.reviews.create(content: 'this is a review', rating: 4, user_id: user.id)
+      review2 = book1.reviews.create(content: 'this is a review', rating: 3, user_id: user.id)
+      review3 = book1.reviews.create(content: 'this is a review', rating: 1, user_id: user.id)
+
+      average_rating = book1.highest_rating.to_i
+
+      expect(average_rating).to eq(review1.rating)
+    end
   end
 end
