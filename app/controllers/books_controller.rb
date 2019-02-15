d5d7d383c888d6408566e5f727e2d03a7dd3e754
manojpanta <manojpanta95@gmail.com>
2019-02-15 12:42:57 -0700

    figured out the problem had with controller and views when there is no reviews for a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,22 +6,23 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @reviews = @book.reviews
-    @average_rating = @book.average_rating
-    @highest_rating = @book.highest_rating
-    @lowest_rating = @book.lowest_rating
 
-    # these methods are working in model tests.. couldnt figure out why not working here.
-    # review_with_lowest_rating is returning nil here but working perfectly in model tests.
-    # help
+    if !@book.reviews.empty? 
 
+      @reviews = @book.reviews
+      @average_rating = @book.average_rating
+      @highest_rating = @book.highest_rating
+      @lowest_rating = @book.lowest_rating
 
-
-    # @highest_reviewer  = @book.review_with_lowest_rating.user.name
-    # @highest_review_content = @book.review_with_highest_rating.content
-    #
-    # @lowest_reviewer  = @book.review_with_lowest_rating.user.name
-    # @lowest_review_content = @book.review_with_lowest_rating.content
+      # these methods are working in model tests.. couldnt figure out why not working here.
+      # review_with_lowest_rating is returning nil here but working perfectly in model tests.
+      # help
+      @highest_reviewer  = @book.review_with_lowest_rating.user.name
+      # @highest_review_content = @book.review_with_highest_rating.content
+      #
+      # @lowest_reviewer  = @book.review_with_lowest_rating.user.name
+      # @lowest_review_content = @book.review_with_lowest_rating.content
+    end
   end
 
 end
